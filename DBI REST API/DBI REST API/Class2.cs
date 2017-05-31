3203b0cf0040fc0312b3e4c291c81f8ab7eb63cb
Leon <llgoal@hotmail.com>
2017-05-31 17:30:46 -0400

    Add files via upload

---
+++ DBI REST API/DBI REST API/Class2.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Http;
+using System.Web.Script.Serialization;
+using System.IO;
+
+namespace DBI_REST_API
+{
+    class Class2
+    {
+        static void Main(string[] args)
+        { 
+            using (HttpClient httpClient = new HttpClient())
+            {
+                // Get Session Id
+                string logonUri = "http://localhost:8000/Api/LogOn/";
+                var logonOptions = new
+                {
+                    accountName = "admin",
+                    password = "123456",
+                    cultureName = string.Empty,
+                    deleteOtherSessions = true,
+                    isWindowsLogOn = false
+                };
+
+                JavaScriptSerializer serializer = new JavaScriptSerializer();
+                var requestBodyAsString = serializer.Serialize(logonOptions);
+                StringContent content =
+                    new StringContent(
+                        requestBodyAsString,
+                        Encoding.UTF8,
+                        "application/json"
+                    );
+
+                string jsonString = string.Empty;
+
+                using (var response = httpClient.PostAsync(logonUri, content).Result)
+                {
+                    jsonString =
+                        response.Content.ReadAsStringAsync().Result;
+                }
+                
+                var obj = (Dictionary<string, object>)serializer.DeserializeObject(jsonString);
+                string sessionId = obj["sessionId"].ToString();
+                string url = "http://localhost:8000/api/Account/588f1498-6fc8-4cdf-8b99-a514da16b0e7/?sessionId=" + sessionId + "";
+                
+                using (var response = httpClient.GetAsync(url).Result)
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine("Success");
+                        
+                        //A <b>Dundas.BI.WebApi.Models.AccountData</b> object containing the account data as a JSON string.
+                        string jsonObject = response.Content.ReadAsStringAsync().Result;
+                        Console.WriteLine(jsonObject);
+                    } else
+                    {
+                        Console.WriteLine(response);
+                    }
+
+                }
+            }
+            Console.Read();
+        }
+        
+    }
+}
---
+++ DBI REST API/DBI REST API/Program.cs
@@ -0,0 +1,85 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Web.Script.Serialization;
+using System;
+using System.Text;
+
+namespace DBI_REST_API
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                string sessionId = GetSessionId(
+                        httpClient,
+                        "http://localhost:8000/",
+                        "admin",
+                        "123456"
+                        );
+                Console.WriteLine(sessionId);
+            }
+            Console.Read();
+        }
+
+
+        private static string GetSessionId(
+            HttpClient client,
+            string baseUri,
+            string username,
+            string password
+        )
+        { 
+
+            string logonUri = baseUri + "Api/LogOn/";
+            string result = string.Empty;
+
+            var logonOptions = new
+            {
+                accountName = username,
+                password = password,
+                cultureName = string.Empty,
+                deleteOtherSessions = false,
+                isWindowsLogOn = false
+
+            };
+
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            var requestBodyAsString = serializer.Serialize(logonOptions);
+
+            StringContent content =
+                new StringContent(
+                    requestBodyAsString,
+                    Encoding.UTF8,
+                    "application/json"
+                    );
+
+            using (var response = client.PostAsync(logonUri, content).Result)
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonString =
+                        response.Content.ReadAsStringAsync().Result;
+
+                    var obj = (System.Collections.Generic.Dictionary<string, object>)serializer.DeserializeObject(jsonString);
+
+                    if (obj["logOnFailureReason"].ToString() == "None")
+                    {
+                        result = obj["sessionId"].ToString();
+                    }
+                    else
+                    {
+                        throw new Exception("Login failed");
+                    }
+
+                }
+
+            }
+
+            return result;
+        }
+    }
+}
